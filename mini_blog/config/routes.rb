fffbc64847b95573c218b0a7057a61c29c0ed6d2
Naoki <naoki826divide@gmail.com>
2016-11-28 20:32:33 +0000

    root変更

--- mini_blog/config/routes.rb
+++ mini_blog/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
       resources :replies
       resources :likes
   end
+   root 'blogs#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- mini_blog/db/migrate/20161128193223_add_likes_count_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddLikesCountToBlogs < ActiveRecord::Migration
-  def change
-    add_column :blogs, :likes_count, :integer
-  end
-end
